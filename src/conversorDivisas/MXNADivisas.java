791af16f3e659195e08aa0541ee8a9c099aa7d2c
RaseckShow <ces_art11@hotmail.com>
2022-12-20 23:47:38 -0600

    Creacion del Repositorio

---
+++ src/conversorDivisas/MXNADivisas.java
@@ -0,0 +1,36 @@
+package conversorDivisas;
+import javax.swing.JOptionPane;
+
+public class MXNADivisas {
+	
+	public void ConvertirPesosADolares(double valor) {
+		double monedaDolar = valor / 19.78;
+		monedaDolar = (double) Math.round(monedaDolar *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Dolares");
+	}
+	
+	public void ConvertirPesosAEuros(double valor) {
+		double monedaEuro = valor / 20.98;
+		monedaEuro = (double) Math.round(monedaEuro *100d)/100;
+		JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Euros");
+	}
+	
+	public void ConvertirPesosALibras(double valor) {
+		double monedaLibra = valor / 24.07;
+        monedaLibra = (double) Math.round(monedaLibra *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Libras Esterlinas");
+	}
+	
+	public void ConvertirPesosAYen(double valor) {
+		double monedaYen = valor / 0.15;
+        monedaYen = (double) Math.round(monedaYen *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Yuanes");
+	}
+	
+	public void ConvertirPesosAWon(double valor) {
+		double monedaWon = valor / 0.015;
+        monedaWon = (double) Math.round(monedaWon *100d)/100;
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Wons");
+	}
+}
+
